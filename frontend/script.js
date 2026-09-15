54cec70989138ac0288f06a741fa065d766227ff
Ashwin <ashwin@gmail.com>
2026-09-15 17:38:39 +0530

    Accessibility Assistant

---
+++ frontend/script.js
@@ -0,0 +1,24 @@
+console.log("FASAL FLOW loaded successfully");
+
+
+// Smooth navigation
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function(event) {
+
+        event.preventDefault();
+
+        const target = document.querySelector(
+            this.getAttribute("href")
+        );
+
+        if (target) {
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+
+    });
+
+});
